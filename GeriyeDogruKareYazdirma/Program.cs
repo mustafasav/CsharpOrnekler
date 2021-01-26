e67de08e50136ed8c55da83a10495c29b198a059
PC <PC@ARGE>
2021-01-26 13:37:18 +0300

    26.01.2021

--- GeriyeDogruKareYazdirma/Program.cs
+++ GeriyeDogruKareYazdirma/Program.cs
@@ -6,7 +6,16 @@ namespace GeriyeDogruKareYazdirma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Bu uygulamada kullanının girmiş olduğu sayıdan geriye doğru her sayının karesini yazdıracağız.
+
+            int girilenSayi = 0;
+            Console.Write("Bir Sayı Giriniz: "); //Kullanıcının Sayı Girmesi İçin Mesaj Veriyoruz.
+            girilenSayi = Convert.ToInt32(Console.ReadLine()); //Kullanıcının girmiş olduğu sayıyı alıp girilenSayi değişkenine aktarıyoruz.
+
+            for (int i = girilenSayi; i > 0; i--) //Girilen sayıdan geriye doğru bir döngü oluşturuyoruz.
+            {
+                Console.WriteLine("Sayı: " + i + "\tKaresi: " + i * i); //Döngü içinde önce sayısı sonra sayının karesini yazdırıyoruz.
+            }
         }
     }
 }
